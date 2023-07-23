4980e643b0b06a394495e668f746f1d72a0a17c4
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-07-23 10:21:40 +0800

    Add form validation before allowing user to play-area

--- script.js
+++ script.js
@@ -12,3 +12,35 @@ playButton.addEventListener("click", function() {
     lobby.classList.remove("hidden");
     lobby.classList.add("visible");
 });
+
+// Access the form.
+const form = document.querySelector("form");
+form.addEventListener('submit', function (event) {
+
+    // Prevent form submission until user's inputs are validated.
+    event.preventDefault();
+
+    // Get player's name.
+    const player1Name = form.querySelector("#player-1-name").value;
+    const player2Name = form.querySelector("#player-2-name").value;
+    
+    // Get player's selected symbols.
+    const player1Symbol = form.querySelector("#player-1-symbol").value;
+    const player2Symbol = form.querySelector("#player-2-symbol").value;
+
+    // Alert if player symbol are the same.
+    if (player1Name === player2Name && player1Symbol === player2Symbol) {
+        alert("You must use different names and symbols");
+    }
+    else if (player1Symbol === player2Symbol) {
+        alert("You must use different symbols");
+    }
+    else if (player1Name === player2Name) {
+        alert("You must use different names");
+    }
+    else {
+        // Hide lobby and popup the play-area.
+        document.querySelector(".lobby").className = "lobby hidden";
+        document.querySelector(".play-area").className = "play-area visible";
+    }
+});
